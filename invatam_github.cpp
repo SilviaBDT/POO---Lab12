1596feb1c05f7bef8a6541bdf200918698f78dd2
SilviaBDT <silvia.brindu@gmail.com>
2016-01-14 22:48:20 +0200

    update2

--- invatam_github.cpp
+++ invatam_github.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <iostream>
 using namespace std;
 
@@ -10,19 +9,7 @@ int main()
   if(n <= 3)
 	  cout << "You're on the good way";
   else 
-	 cout << "You need another one";
+	 cout << "You need another one, bro!";
   return 0;
 }
-=======
-#include <iostream>
-using namespace std;
 
-int main()
-{
-  int n;
-  cout << "The number of coffee you had today is: " << endl;
-  cin >> n;
-  cout << "You need another one";
-  return 0;
-}
->>>>>>> origin/master
