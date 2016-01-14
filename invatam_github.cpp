57412f1c9d945773da4e35e670feb5a96feb05f0
SilviaBDT <silvia.brindu@gmail.com>
2016-01-14 22:29:46 +0200

    Update and rename ma-ta.c to invatam_github.cpp

---
+++ invatam_github.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int n;
+  cout << "The number of coffee you had today is: " << endl;
+  cin >> n;
+  cout << "You need another one";
+  return 0;
+}
